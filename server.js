1b35d2871f530ed1d61da9268bfbf26de3917098
Yvtsan <yvtsanlevy>
2018-11-21 22:39:15 +0200

    new commit

--- server.js
+++ server.js
@@ -1,28 +1,40 @@
-const express= require('express');
-const hbs =require('hbs');
+const express = require('express');
+const hbs = require('hbs');
 
+var app = express();
 
-var app= express();
-
-hbs.registerPartials(__dirname + '/wiews/partial')
-app.set('views', __dirname + '/views');
-app.set('view engine', 'hbs')
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req,res) => {
-res.send('<h1>hello express!<h1>')
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
 });
 
-app.get('/about', (req,res) => {
-res.render('about.hbs', {
-	pageTitle: 'about page',
-	currentYear: new Date().getFullYear()
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
 
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
   });
 });
-app.get('/bad', (req,res) => {
-res.send({
-	errorMessage : 'unable to handle request'
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
   });
 });
-app.listen(3000);
+
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
